dacc436c77f421a4fe690131696910fb32db0a2a
Berke <69588559+berke-volkan@users.noreply.github.com>
2023-11-26 18:00:18 +0300

    Update page.tsx

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -1,75 +1,67 @@
-"use client"
+"use client
 
 import React, { useState, useEffect } from 'react';
 import { useUser } from '@clerk/clerk-react';
 import { Heading } from '@/components/heading';
 import { CodeIcon } from 'lucide-react';
-import Empty from '@/components/empty';
+import { collection, addDoc, onSnapshot } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBq0UtNaMQ9W2yrOakjutO47WZjJgH4bUw",
+  authDomain: "devoloperz.firebaseapp.com",
+  projectId: "devoloperz",
+  storageBucket: "devoloperz.appspot.com",
+  messagingSenderId: "851290003802",
+  appId: "1:851290003802:web:4761f04f7a1b4b63273b63"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
 
 const App = () => {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
-  const [ws, setWs] = useState(null);
   const { user } = useUser();
 
   useEffect(() => {
-    const socket = new WebSocket('ws://localhost:3001');
+    const messagesRef = collection(firestore, 'messages');
 
-    socket.addEventListener('open', () => {
-      console.log('Connected to server');
-      setWs(socket);
+    const unsubscribe = onSnapshot(messagesRef, (snapshot) => {
+      const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setMessages(data);
     });
 
-    socket.addEventListener('message', (event) => {
-      const messageBlob = event.data;
+    return () => unsubscribe();
+  }, [firestore]);
 
-      // Blob verisini bir metin olarak almak için aşağıdaki gibi kullanabilirsiniz.
-      messageBlob.text().then((text) => {
-        console.log('Text content of Blob:', text);
-
-        try {
-          // JSON.parse işlemi yapabilirsiniz.
-          const parsedMessage = JSON.parse(text);
-
-          // Mesajları güncelleyebilirsiniz.
-          setMessages((prevMessages) => [...prevMessages, parsedMessage]);
-        } catch (error) {
-          console.error('Error parsing JSON:', error);
-        }
-      });
-    });
-
-    return () => {
-      socket.close();
-    };
-  }, []);
-
-  const handleSendMessage = () => {
-    if (!ws || newMessage.trim() === '') {
+  const handleSendMessage = async () => {
+    if (newMessage.trim() === '') {
       return;
     }
 
     const message = {
       text: newMessage,
       timestamp: new Date().toISOString(),
-      username: user.firstName.substring(0, 5), // Kullanıcının adının ilk 5 harfini alabilirsiniz
+      username: user.firstName.substring(0, 5),
     };
 
-    // Gelen mesajı sunucuya gönderiyoruz
-    ws.send(JSON.stringify(message));
+    const messagesRef = collection(firestore, 'messages');
+    await addDoc(messagesRef, message);
 
     setNewMessage('');
   };
 
   return (
     <div className="container mx-auto p-4">
-        <Heading
-            title="Coding Room - Web3"
-            description="Blockchain based coding room"
-            icon={CodeIcon}
-            iconColor="text-orange-700"
-            bgColor="bg-orange-700/10"
-        />
+      <Heading
+        title="Coding Room - Web3"
+        description="Blockchain based coding room"
+        icon={CodeIcon}
+        iconColor="text-orange-700"
+        bgColor="bg-orange-700/10"
+      />
       <div className="flex items-center space-x-4">
         <input
           type="text"
@@ -82,27 +74,20 @@ const App = () => {
           Send
         </button>
       </div>
-      { messages.length > 0 &&
       <div className="max-h-96 overflow-y-auto bg-gray-200 p-4 rounded-lg mb-4 mt-4">
-        {messages.map((message, index) => (
-          <div key={index} className="mb-2">
-           {message.username}  - <span className="text-gray-500">{message.timestamp}</span> - {message.text}
+        {messages.map((message) => (
+          <div key={message.id} className="mb-2">
+            {message.username} - <span className="text-gray-500">{message.timestamp}</span> - {message.text}
           </div>
         ))}
       </div>
-     }
-     {messages.length === 0 &&
-     <Empty 
-     label="Not web 3.0 yet."/>
-
-     }
-
     </div>
   );
 };
 
 const AuthenticatedApp = () => (
-  <App/>
+  <App />
 );
 
 export default AuthenticatedApp;
+
