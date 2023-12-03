c571419d4cca5a9dd71de184577d9c6cf8825064
Berke <69588559+berke-volkan@users.noreply.github.com>
2023-12-03 14:05:57 +0300

    Update page.tsx

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -6,34 +6,6 @@ import { useRouter } from "next/navigation";
 
 const tools=[
   {
-    label:"General Chat",
-    icon:MessageSquare,
-    href:"/general-chat",
-    color:"text-violet-500",
-    bgColor:"bg-violet-500/10"
-  },  
-  {
-    label:"Music Generation",
-    icon:Music,
-    href:"/music",
-    color:"text-emerald-500",
-    bgColor:"bg-emerald-500/10"
-  },
-  {
-    label:"Image Generation",
-    icon:ImageIcon,
-    href:"/image",
-    color:"text-pink-700",
-    bgColor:"bg-pink-700/10"
-  },  
-  {
-    label:"Video Generation",
-    icon:VideoIcon,
-    href:"/video",
-    color:"text-orange-700",
-    bgColor:"bg-orange-700/10"
-  },  
-  {
     label:"Code Generation",
     icon:Code,
     href:"/code",
