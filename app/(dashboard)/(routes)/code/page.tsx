43ebf5bd6f223ffc1cf067e25e1cb9a17a7d1e41
Berke <69588559+berke-volkan@users.noreply.github.com>
2023-12-15 15:47:40 +0300

    Update page.tsx

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -1,4 +1,5 @@
-"use client"
+"use client
+
 import { useRef } from 'react';
 import Editor, { OnMount } from "@monaco-editor/react";
 import * as y from 'yjs';
@@ -9,21 +10,18 @@ import * as monaco from 'monaco-editor';
 const EditorComponent = () => {
   const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
 
-  const handleMount = (editor) => {
-  editorRef.current = editor;
-  const doc = new y.Doc();
-  const provider = new WebrtcProvider("test-room", doc);
-  const type = doc.getText("monaco");
-  
-  // getModel() çağrısının bir ITextModel döndürdüğünden emin olun.
-  const model = editor.getModel();
-  if (model) {
-    new MonacoBinding(type, model, new Set([editor]), provider.awareness);
-  } else {
-    // model null ise, bir hata mesajı gösterebilir veya başka bir işlem yapabilirsiniz.
-    console.error('Editor model is null.');
-  }
-};
+  const handleMount: OnMount = (editor: monaco.editor.IStandaloneCodeEditor) => {
+    editorRef.current = editor;
+    const doc = new y.Doc();
+    const provider = new WebrtcProvider("test-room", doc);
+    const type = doc.getText("monaco");
+    const model = editor.getModel();
+    if (model) {
+      new MonacoBinding(type, model, new Set([editor]), provider.awareness);
+    } else {
+      console.error('Editor model is null.');
+    }
+  };
 
   return (
     <Editor
