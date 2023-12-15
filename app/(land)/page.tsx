aada3385ab2d762cb6059032e308fac76d161fd3
Berke <69588559+berke-volkan@users.noreply.github.com>
2023-12-15 23:53:00 +0300

    Analytics Companent

--- app/(land)/page.tsx
+++ app/(land)/page.tsx
@@ -1,10 +1,11 @@
 import LandingContent from "@/components/landing-content";
 import { LandingHero } from "@/components/landing-hero";
 import { LandingNavBar } from "@/components/landing-navbar";
-
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   return (
+    <Analytics />
     <div className="h-full ">
       <LandingNavBar/>
       <LandingHero/>
