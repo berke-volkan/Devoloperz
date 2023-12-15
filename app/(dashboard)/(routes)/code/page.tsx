e021695cdfb3baac25a7f557211fa43f1c2db6b3
Berke <69588559+berke-volkan@users.noreply.github.com>
2023-12-15 15:22:23 +0300

    Update page.tsx

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -3,26 +3,28 @@
 import Editor from "@monaco-editor/react";
 import { useRef } from 'react';
 import * as y from 'yjs';
-import {WebrtcProvider} from 'y-webrtc'
-import {MonacoBinding} from 'y-monaco'
-const editor=()=>{
-    const editorRef = useRef(null);
-    function handleMount(editor){
-        editorRef.current = editor;
-        const doc=new y.Doc();
-        const provider= new WebrtcProvider("test-room",doc)
-        const type=doc.getText("monaco");
-        const binding=new MonacoBinding(type,editorRef.current.getModel(),new Set([editorRef.current]),provider.awareness);
-    }
-    return (
-        <Editor 
-        height="100vh"
-        width="100vh"
-        theme="vs-dark"
-        onMount={handleMount}
-        />
-    )
-}
+import { WebrtcProvider } from 'y-webrtc';
+import { MonacoBinding } from 'y-monaco';
 
+const EditorComponent = () => {
+  const editorRef = useRef(null);
 
-export default editor
+  function handleMount(editor) {
+    editorRef.current = editor;
+    const doc = new y.Doc();
+    const provider = new WebrtcProvider("test-room", doc);
+    const type = doc.getText("monaco");
+    const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness);
+  }
+
+  return (
+    <Editor
+      height="100vh"
+      width="100vh"
+      theme="vs-dark"
+      onMount={handleMount}
+    />
+  );
+};
+
+export default EditorComponent;
