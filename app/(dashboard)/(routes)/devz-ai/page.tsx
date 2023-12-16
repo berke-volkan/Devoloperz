7e0f83d7d10505146ea63b757bb987f4db07bb56
berke-volkan <berkevolkan0909878909@gmail.com>
2023-12-16 19:28:52 +0300

    Ai

--- app/(dashboard)/(routes)/devz-ai/page.tsx
+++ app/(dashboard)/(routes)/devz-ai/page.tsx
@@ -17,90 +17,110 @@ import { cn } from "@/lib/utils";
 import { BotAvatar } from "@/components/bot-avatar";
 import {UserAvatar} from "@/components/user-avatar";
 let msg: string[] = [];
-const CınversationPage = () => {
-    const Router=useRouter();
-    const form=useForm<z.infer<typeof FormSchema>>({
-        resolver:zodResolver(FormSchema),
-        defaultValues:{
-            prompt:""
-        }
-    })
-    const isLoading =form.formState.isSubmitting
-    const onSubmit = async (data: z.infer<typeof FormSchema>) =>{
-        try{
-            let message = data.prompt;
-            const response = await axios.post("/api/ai",{
-                messages:message,
-            })
-            msg.push(message)
-            msg.push(response.data)
-            
-            form.reset();
-        }catch(error:any){
-            console.log(error);
-        }finally{
-            form.reset();
-            Router.refresh();
-        }
-            
-    };
-    return(
-        <div>
-            <Heading
-            title="Devz-Ai"
-            description="Create your apps faster with AI !   Powered by Cortex"
-            icon={AtomIcon}
-            iconColor="text-yellow-700"
-            bgColor="bg-yellow-700/10"
-            />
-            <div className="px-4 lg:px-8">
-                <div>
-                    <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)} className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2">
-                        <FormField
-                        name="prompt"
-                        render={({ field }) => (
-                            <FormItem className="col-span-12 lg:col-span-10">
-                                <FormControl className="m-0 p-0">
-                                <Input className="border-0 outline-none focus-visible: ring-0 focus-visible: ring-transparent" disabled={isLoading} placeholder="How con ı create a snake game in python?" {...field}/>
-                                </FormControl>
-                            </FormItem>
-                        )}
-                        /> 
-                        <Button className="col-span-12 lg:col-span-2 w-full"disabled={isLoading} onClick={form.handleSubmit(onSubmit)}>
-                            Ask!
-                        </Button>
-                     </form>             
-                    </Form>
-                </div>
-                <div className="space-y-4 mt-4">
-                    {isLoading && (
-                      <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
-                        <LoaderNesia/>
-                      </div>  
-                    )}
-                    {msg.length ===0 && !isLoading && (
-                        <Empty 
-                        label="No messages yet."/>
-                    )}
-                    <div className="flex flex-col-reverse gap-y-4">
-                        {msg.map((message, index) => (
-                            <div 
-                            key={index}
-                            className={cn("p-8 w-full flex item-start gap-x-8 rounded-lg",index % 2 === 0 ? "bg-white border border-black/10" : "bg-muted")}>
-                            
-                            {index % 2 !== 0 ? <BotAvatar/> : <UserAvatar/>}
-                            <p className="text-sm">
-                            {message}
-                            </p>
-                            
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </div>
-    )   
-}
+// Import statements
 
-export default CınversationPage
+const ConversationPage = () => {
+    const router = useRouter();
+    const form = useForm<z.infer<typeof FormSchema>>({
+      resolver: zodResolver(FormSchema),
+      defaultValues: {
+        prompt: "",
+      },
+    });
+  
+    const isLoading = form.formState.isSubmitting;
+    const [msg, setMsg] = useState<string[]>([]);
+  
+    const onSubmit = async (data: z.infer<typeof FormSchema>) => {
+        try {
+          let message = data.prompt;
+          console.log(message); // Corrected: Log the 'message' variable
+      
+          const response = await axios.post("/api/ai", {
+            messages: message,
+          });
+      
+          setMsg((prevMsg) => [...prevMsg, message, response.data]);
+      
+          form.reset();
+        } catch (error) {
+          console.error("[api_err]", error);
+      
+          // Handle errors appropriately, e.g., show a user-friendly message
+        } finally {
+          form.reset();
+          router.refresh();
+        }
+      };
+      
+  
+    return (
+      <div>
+        <Heading
+          title="Devz-Ai"
+          description="Create your apps faster with AI! Powered by Cortex"
+          icon={AtomIcon}
+          iconColor="text-yellow-700"
+          bgColor="bg-yellow-700/10"
+        />
+        <div className="px-4 lg:px-8">
+          <div>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+              >
+                <FormField
+                  name="prompt"
+                  render={({ field }) => (
+                    <FormItem className="col-span-12 lg:col-span-10">
+                      <FormControl className="m-0 p-0">
+                        <Input
+                          className="border-0 outline-none focus-visible: ring-0 focus-visible: ring-transparent"
+                          disabled={isLoading}
+                          placeholder="How can I create a snake game in python?"
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <Button
+                  className="col-span-12 lg:col-span-2 w-full"
+                  disabled={isLoading}
+                  onClick={form.handleSubmit(onSubmit)}
+                >
+                  Ask!
+                </Button>
+              </form>
+            </Form>
+          </div>
+          <div className="space-y-4 mt-4">
+            {isLoading && (
+              <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+                <LoaderNesia />
+              </div>
+            )}
+            {msg.length === 0 && !isLoading && <Empty label="No messages yet." />}
+            <div className="flex flex-col-reverse gap-y-4">
+              {msg.map((message, index) => (
+                <div
+                  key={index}
+                  className={cn(
+                    "p-8 w-full flex item-start gap-x-8 rounded-lg",
+                    index % 2 === 0 ? "bg-white border border-black/10" : "bg-muted"
+                  )}
+                >
+                  {index % 2 !== 0 ? <BotAvatar /> : <UserAvatar />}
+                  <p className="text-sm">{message}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default ConversationPage;
+  
--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -1,28 +1,31 @@
-
 import { NextResponse } from "next/server";
 import { OpenAI } from "openai";
 
 const openai = new OpenAI({
-    apiKey: "sk-61B0p5iYw4YKZwY20fmOAp4qxQtT4TLb",
+    apiKey: "this_is_magic_key",
     baseURL: "https://api.h10.pro",
 });
 
 export async function POST(req: Request) {
     try {
-        const body = await req.json(); // Add await here to properly parse the JSON body
-        const messages = body.messages; // Fix here to properly access messages
+        // Parse JSON body directly in the destructuring assignment
+        const { messages } = await req.json();
 
-            const response = await openai.chat.completions.create({
-                model: "gpt-4-1106-preview",
-                messages: [
-                    { role: "user", content: messages }, // Fix the extra double quote here
-                ],
-                temperature: 0,
-            });
-            return NextResponse.json(response.choices[0].message.content);
+        const response = await openai.chat.completions.create({
+            model: "gpt-4-1106-preview",
+            messages: [
+                { role: "user", content: messages },
+            ],
+            temperature: 0,
+        });
+
+        // Return the content directly from the response
+        return NextResponse.json(response.choices[0].message.content);
         
-    } catch (e) {
-        console.log("[api_err]", e);
+    } catch (error) {
+        console.error("[api_err]", error);
+        // Instead of a generic "Internal Error," you might want to include more information
+        // in the response, such as the error message.
         return new NextResponse("Internal Error", { status: 500 });
     }
 }
