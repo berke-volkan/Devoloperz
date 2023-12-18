6987d2ef02f8cb8a27a6ccde8fdbe18bf55482b1
berke-volkan <berkevolkan0909878909@gmail.com>
2023-12-18 19:56:23 +0300

    Chat Page - UI update

--- app/(dashboard)/(routes)/chat/page.tsx
+++ app/(dashboard)/(routes)/chat/page.tsx
@@ -2,11 +2,15 @@
 import React, { useState, useEffect } from 'react';
 import { useUser } from '@clerk/clerk-react';
 import { Heading } from '@/components/heading';
-import { BadgeCheck, BadgeCheckIcon, CodeIcon, MessageSquare, Shield } from 'lucide-react';
+import { Shield, BadgeCheckIcon, MessageSquare, CheckCircleIcon } from 'lucide-react';
 import { getDatabase, ref, onValue, push } from 'firebase/database';
 import { initializeApp } from "firebase/app";
-import { UserAvatar } from '@/components/user-avatar';
 import { cn } from '@/lib/utils';
+import { User as NextUser } from '@nextui-org/user';
+import {Badge} from "@nextui-org/badge"
+import {Spacer} from "@nextui-org/spacer"
+import { Card, CardContent, CardDescription } from '@/components/ui/card';
+import {Popover, PopoverTrigger, PopoverContent} from "@nextui-org/popover";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBq0UtNaMQ9W2yrOakjutO47WZjJgH4bUw",
@@ -16,22 +20,134 @@ const firebaseConfig = {
   messagingSenderId: "851290003802",
   appId: "1:851290003802:web:4761f04f7a1b4b63273b63"
 };
-// Initialize Firebase
-// Initialize Firebase
+// Firebase'i başlat
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);  // Get the database reference
+const database = getDatabase(app);
+const RenderMessage: React.FC<{ message: Message }> = ({ message }) => {
+  if (message.lastname === "Admin") {
+    return (
+      <div>
+        <Popover showArrow
+      backdrop="opaque"
+      placement="right"
+      classNames={{
+        base: [  
+          // arrow color
+          "before:bg-default-200"
+        ],
+        content: [
+          "py-3 px-4 border border-default-200",
+          "bg-gradient-to-br from-black to-default-gray",
+          "dark:from-default-100 dark:to-default-50",
+        ],
+      }}>
+          <PopoverTrigger>
+        <Badge
+          isOneChar
+          content={<Shield className="text-green-500" />}
+          color="success"
+          placement="top-right"
+        >
+          
+          <Spacer x={4} />
+        </Badge>
+        </PopoverTrigger>
+        <PopoverContent>
+        <div className="px-1 py-2 ">
+          <div className="text-small font-bold text-white">Staff Badge</div>
+          <div className="text-tiny text-white">This user  protecting our community from toxic peoples.  </div>
+          <div className="text-tiny text-white">Apply as a staff from our email!</div>
+        </div>
+
+        </PopoverContent>
+        </Popover>
+        <NextUser
+          name={message.username}
+          description={message.lastname}
+          avatarProps={{
+            src: message.avatar,
+          }}
+        />
+        <Spacer x={4}/>
+        <span className="text-gray-500">{message.timestamp}</span> - {message.text}
+      </div>
+    );
+  } else if (message.lastname === "Verified") {
+    return (
+      <div>
+      <Popover showArrow
+    backdrop="opaque"
+    placement="right"
+    classNames={{
+      base: [  
+        // arrow color
+        "before:bg-default-200"
+      ],
+      content: [
+        "py-3 px-4 border border-default-200",
+        "bg-gradient-to-br from-black to-default-gray",
+        "dark:from-default-100 dark:to-default-50",
+      ],
+    }}>
+        <PopoverTrigger>
+      <Badge
+        isOneChar
+        content={<BadgeCheckIcon className="text-green-500" />}
+        color="success"
+        placement="top-right"
+      >
+        
+        <Spacer x={4} />
+      </Badge>
+      </PopoverTrigger>
+      <PopoverContent>
+      <div className="px-1 py-2 ">
+        <div className="text-small font-bold text-white">Verified Badge</div>
+        <div className="text-tiny text-white">This user is verified  </div>
+        <div className="text-tiny text-white">This user showed his talent!</div>
+      </div>
+
+      </PopoverContent>
+      </Popover>
+      <NextUser
+        name={message.username}
+        description={message.lastname}
+        avatarProps={{
+          src: message.avatar,
+        }}
+      />
+      <Spacer x={4}/>
+      <span className="text-gray-500">{message.timestamp}</span> - {message.text}
+    </div>
+    );
+  } else {
+    return (
+      <div>
+        <NextUser
+          name={message.username}
+          description={message.lastname}
+          avatarProps={{
+            src: message.avatar,
+          }}
+        />
+        <Spacer x={4}/>
+        <span className="text-gray-500">{message.timestamp}</span> - {message.text}
+      </div>
+    );
+  }
+};
+
 interface Message {
   id: string;
   text: string;
   timestamp: string;
   username: string;
-  lastname: string; // Add the 'lastname' property to the Message type
-  // Add any other properties you might have
+  lastname: string;
+  avatar: string;
 }
 
 const App = () => {
   const [messages, setMessages] = useState<Message[]>([]);
-
   const [newMessage, setNewMessage] = useState('');
   const { user } = useUser();
 
@@ -43,8 +159,9 @@ const App = () => {
     const message = {
       text: newMessage,
       timestamp: new Date().toISOString(),
-      username: user.firstName?.substring(0, 5), // Add a nullish coalescing operator
-      lastname: user.lastName,
+      username: user.firstName?.substring(0, 5) ?? "anonim",
+      lastname: user.lastName ?? "",
+      avatar: user.profileImageUrl ?? "",
     };
 
     const messagesRef = ref(database, 'messages');
@@ -53,62 +170,59 @@ const App = () => {
     setNewMessage('');
   };
 
-useEffect(() => {
-  const msgRef = ref(database, 'messages');
-  const unsubscribe = onValue(msgRef, (snapshot) => {
-    const data = snapshot.val();
-    const formattedData = data
-      ? Object.keys(data).map((id) => ({ id, ...data[id] }))
-      : [];
-    setMessages(formattedData);
-  });
+  useEffect(() => {
+    const msgRef = ref(database, 'messages');
+    const unsubscribe = onValue(msgRef, (snapshot) => {
+      const data = snapshot.val();
+      const formattedData = data
+        ? Object.keys(data).map((id) => ({ id, ...data[id] }))
+        : [];
+      setMessages(formattedData);
+    });
 
-  return () => unsubscribe();
-}, []); // Empty dependency array indicates no dependencies
-
+    return () => unsubscribe();
+  }, []);
 
   return (
     <>
-    <div className="container mx-auto p-4">
-      <Heading
-        title="Chat Room"
-        description="Chat With Devs!"
-        icon={MessageSquare}
-        iconColor="text-green-700"
-        bgColor="bg-green-700/10"
-      />
-      
+      <div className="container mx-auto p-4">
+        <Heading
+          title="Chat Room"
+          description="Chat With Devs!"
+          icon={MessageSquare}
+          iconColor="text-green-700"
+          bgColor="bg-green-700/10"
+        />
+        
         {messages.map((message) => (
-         <div 
-  key={message.id}
-  className={cn(
-    "p-10 w-full flex item-start gap-x-10 mt-4 rounded-lg",
-    message.username !== (user?.firstName?.substring(0, 5) ?? "anonim") 
-      ? "bg-white border border-black/10" 
-      : "bg-muted"
-  )}
->
-  {message.lastname === "Admin" && <Shield className="text-green-500"/> }
-  {message.lastname === "Verified" && <BadgeCheck className="text-green-500" /> }
-  <UserAvatar />{message.username} - <span className="text-gray-500">{message.timestamp}</span> - {message.text}
-</div>
+          <div 
+            key={message.id}
+            className={cn(
+              "p-10 w-full flex item-start gap-x-10 mt-4 rounded-lg",
+              message.username !== (user?.firstName?.substring(0, 5) ?? "anonim") 
+                ? "bg-white border border-black/10" 
+                : "bg-muted"
+            )}
+          >
+        
+            <RenderMessage message={message} />
+          </div>
         ))}
-    </div>
-    <div className="flex items-center space-x-4">
-    <input
-      type="text"
-      placeholder="Type your message"
-      value={newMessage}
-      onChange={(e) => setNewMessage(e.target.value)}
-      className="p-2 border border-gray-300 rounded-lg w-full"
-    />
-    <button onClick={handleSendMessage} className="p-2 bg-blue-500 text-white rounded-lg">
-      Send
-    </button>
-  </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <input
+          type="text"
+          placeholder="Type your message"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          className="p-2 border border-gray-300 rounded-lg w-full"
+        />
+        <button onClick={handleSendMessage} className="p-2 bg-blue-500 text-white rounded-lg">
+          Send
+        </button>
+      </div>
     </>
   );
-  
 };
 
 const AuthenticatedApp = () => (
---
+++ components/Checkİcon.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+interface CheckIconProps {
+  size?: number;
+  height?: number;
+  width?: number;
+  [x: string]: any; // Diğer tüm geçerli SVG özelliklerini kabul etmek için
+}
+
+export const CheckIcon: React.FC<CheckIconProps> = ({
+  size,
+  height,
+  width,
+  ...props
+}) => {
+  return (
+    <svg
+      width={size || width || 18}
+      height={size || height || 18}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M7.75 11.9999L10.58 14.8299L16.25 9.16992"
+        stroke="currentColor"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
