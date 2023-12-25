7faf9566e99dbb36cdd2c1804e1e62f2b6084b97
Berke <69588559+berke-volkan@users.noreply.github.com>
2023-12-25 17:20:31 +0300

    Update page.tsx

--- app/(dashboard)/(routes)/minigames/page.tsx
+++ app/(dashboard)/(routes)/minigames/page.tsx
@@ -8,13 +8,13 @@ import { Empty } from '@/components/empty';
 const Sidebar = () => {
   const pathname = usePathname();
   const user = useUser();
-
+  
   return (
     <div className="container mx-auto p-4">
       <div className="container mx-auto p-4">
         <Heading
           title="Minigames!"
-          description="Lets fun!"
+          description="Lets have fun!"
           icon={Swords}
           bgColor="bg-red-700/10"
         />
@@ -22,7 +22,21 @@ const Sidebar = () => {
       </div>
       <div>
         {/* Move the script inclusion to the head of your HTML document */}
-        <script src="https://cdn.htmlgames.com/embed.js?game=RedAndGreen2&amp;bgcolor=white"></script>
+        {/* Start of Tawk.to Script */}
+        <script type="text/javascript">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/65898e8a70c9f2407f833972/1higl9jv8';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </script>
+        {/* End of Tawk.to Script */}
       </div>
     </div>
   );
