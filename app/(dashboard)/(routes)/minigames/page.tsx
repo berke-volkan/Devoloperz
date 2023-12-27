0df63feeba59b38b4f7e899acfaa59f7a5edee32
Berke <69588559+berke-volkan@users.noreply.github.com>
2023-12-27 18:07:27 +0300

    Update page.tsx

--- app/(dashboard)/(routes)/minigames/page.tsx
+++ app/(dashboard)/(routes)/minigames/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { usePathname } from "next/navigation";
 import { useUser } from "@clerk/nextjs";
 import { Heading } from '@/components/heading';
@@ -11,20 +10,27 @@ const Sidebar = () => {
   
   return (
     <div className="container mx-auto p-4">
-      <div className="container mx-auto p-4">
-        <Heading
-          title="Minigames!"
-          description="Lets have fun!"
-          icon={Swords}
-          bgColor="bg-red-700/10"
-        />
-        <Empty label="Nothing to see yet :(" />
+      <Heading
+        title="Minigames!"
+        description="Lets have fun!"
+        icon={Swords}
+        bgColor="bg-red-700/10"
+      />
+      <Empty label="Nothing to see yet :(" />
+      {/* iDev.Games Responsive Embed Code for SillySnake */}
+      <div style={{ position: 'relative', height: '0', overflow: 'hidden', paddingBottom: '56.25%' }}>
+        <iframe 
+          id="embededGame" 
+          src="https://idev.games/embed/sillysnake" 
+          scrolling="no" 
+          seamless="seamless" 
+          frameBorder="0" 
+          style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }}
+        >
+          Browser not compatible.
+        </iframe>
       </div>
-      <!-- iDev.Games Responsive Embed Code for SillySnake -->
-<div style="position: relative;height: 0;overflow: hidden;padding-bottom: 56.25%;">
-    <iframe id="embededGame" src="https://idev.games/embed/sillysnake" scrolling="no" seamless="seamless" frameBorder="0" style="position: absolute;top:0;left: 0;width: 100%;height: 100%;">Browser not compatible.</iframe>
-</div>
-<!-- End Embed Code -->
+      {/* End Embed Code */}
     </div>
   );
 };
