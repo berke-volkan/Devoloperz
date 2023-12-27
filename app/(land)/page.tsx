2611d72b2d4602b73859b365a14deb5ef95fd2d8
Berke <69588559+berke-volkan@users.noreply.github.com>
2023-12-27 20:00:26 +0300

    Update page.tsx

--- app/(land)/page.tsx
+++ app/(land)/page.tsx
@@ -5,15 +5,11 @@ import { LandingNavBar } from "@/components/landing-navbar";
 
 export default function Home() {
   return (
-    <main className="h-full bg-[#111827] overflow-auto">
-    <div className="mx-auto max-w-screen-xl h-full w-full">
      <div className="h-full">
       <LandingNavBar />
       <LandingHero />
-      
-     </div>
-                </div>
       <LandingContent />
-        </main>
+     </div>
+      
   );
 }
