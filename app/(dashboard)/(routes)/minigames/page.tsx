58c2af24f7eca2cd2ad677292994f4faa00f6bdd
Berke <69588559+berke-volkan@users.noreply.github.com>
2023-12-27 18:34:10 +0300

    Update page.tsx

--- app/(dashboard)/(routes)/minigames/page.tsx
+++ app/(dashboard)/(routes)/minigames/page.tsx
@@ -28,8 +28,8 @@ const Sidebar = () => {
         </iframe>
       </div>
       <div style="position: relative;height: 0;overflow: hidden;padding-bottom: 56.25%;">
-    <iframe id="embededGame" src="https://idev.games/embed/catch-em-all" scrolling="no" seamless="seamless" frameBorder="0" style="position: absolute;top:0;left: 0;width: 100%;height: 100%;">Browser not compatible.</iframe>
-</div>
+          <iframe id="embededGame" src="https://idev.games/embed/catch-em-all" scrolling="no"  frameBorder="0" style="position: absolute;top:0;left: 0;width: 100%;height: 100%;">Browser not compatible.</iframe>
+m     </div>
       {/* End Embed Code */}
     </div>
   );
