fe9ab079c2d885a2d56f3cdf5cbdd5989a3e02b5
Berke <69588559+berke-volkan@users.noreply.github.com>
2023-12-27 19:37:51 +0300

    Update landing-content.tsx

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -15,7 +15,7 @@ export const LandingContent = () => {
                 <Card className="max-w-[340px]">
       <CardHeader className="justify-between">
         <div className="flex gap-5">
-          <Avatar isBordered radius="full" size="md" src="/avatars/avatar-1.png" />
+          <Avatar isBordered radius="full" size="md" src="/logo.png" />
           <div className="flex flex-col gap-1 items-start justify-center">
             <h4 className="text-small font-semibold leading-none text-default-600">Zoey Lang</h4>
             <h5 className="text-small tracking-tight text-default-400">@zoeylang</h5>
