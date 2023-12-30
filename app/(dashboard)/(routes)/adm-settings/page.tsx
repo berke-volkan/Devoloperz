5cacf05c431e7e187557dc9cb170b20300c4808f
berke-volkan <berkevolkan0909878909@gmail.com>
2023-12-30 12:02:08 +0300

    .

---
+++ app/(dashboard)/(routes)/adm-settings/page.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Slider } from "@nextui-org/react";
+import { PowerIcon, PowerOffIcon } from "lucide-react";
+import firebase from "firebase/app";
+import "firebase/database"; // Firebase Realtime Database için gerekli olan modül
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue, update, Database } from 'firebase/database';
+const firebaseConfig = {
+    apiKey: "AIzaSyBq0UtNaMQ9W2yrOakjutO47WZjJgH4bUw",
+    authDomain: "devoloperz.firebaseapp.com",
+    projectId: "devoloperz",
+    storageBucket: "devoloperz.appspot.com",
+    messagingSenderId: "851290003802",
+    appId: "1:851290003802:web:4761f04f7a1b4b63273b63"
+  };
+  // Firebase'i başlat
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const App = () => {
+  const handleSliderChange = (value:any) => {
+    // Firebase veritabanındaki "chatMode" adlı bir alanı güncelle
+    const linkRef = ref(database, `bakim/chat`); // Doğru referans yolu
+    update(linkRef, { value: value.toString() }) // Güncelleme işlemi
+  };
+
+  return (
+    <Slider
+      label="Chat Mode (0 Açık - 1 Kapalı)"
+      step={1}
+      maxValue={1}
+      minValue={0}
+      defaultValue={0}
+      className="max-w-md"
+      startContent={<PowerIcon className="text-2xl" />}
+      endContent={<PowerOffIcon className="text-2xl" />}
+      onChange={handleSliderChange} // Slider değeri değiştiğinde çağrılacak fonksiyon
+    />
+  );
+};
+
+export default App;
