ef5cb01ca1fd4f0c38881c3d2cfece248c92bc5a
Berke <69588559+berke-volkan@users.noreply.github.com>
2024-01-13 00:23:05 +0300

    Update page.tsx

--- app/adm-settings/page.tsx
+++ app/adm-settings/page.tsx
@@ -1,80 +1,37 @@
-"use client";
 import React from "react";
-import { Slider } from "@nextui-org/react";
-import { PowerIcon, PowerOffIcon } from "lucide-react";
-import firebase from "firebase/app";
-import "firebase/database"; // Firebase Realtime Database için gerekli olan modül
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, onValue, update, Database } from 'firebase/database';
-import {Tooltip} from "@nextui-org/tooltip";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBq0UtNaMQ9W2yrOakjutO47WZjJgH4bUw",
-    authDomain: "devoloperz.firebaseapp.com",
-    projectId: "devoloperz",
-    storageBucket: "devoloperz.appspot.com",
-    messagingSenderId: "851290003802",
-    appId: "1:851290003802:web:4761f04f7a1b4b63273b63"
-  };
-  // Firebase'i başlat
-// Firebase'i başlat
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
+import {Slider} from "@nextui-org/react";
 
 export default function App() {
-  const [value, setValue] = React.useState(0.2);
-  const [inputValue, setInputValue] = React.useState("0.2");
-
-  const handleChange = (value:any) => {
-    if (isNaN(Number(value))) return;
-
-    setValue(value);
-    setInputValue(value.toString());
-  };
-
   return (
-    <Slider 
-      label="Temperature" 
-      size="sm"
-      step={0.01} 
-      maxValue={1} 
-      minValue={0} 
-      color="foreground"
-      classNames={{
-        base: "max-w-md",
-        label: "text-medium",
-      }}
-      // we extract the default children to render the input
-      renderValue={({children, ...props}) => (
-        <output {...props}>
-          <Tooltip
-            className="text-tiny text-default-500 rounded-md"
-            content="Press Enter to confirm"
-            placement="left"
-          >
-            <input
-              className="px-1 py-0.5 w-12 text-right text-small text-default-700 font-medium bg-default-100 outline-none transition-colors rounded-small border-medium border-transparent hover:border-primary focus:border-primary"
-              type="text"
-              aria-label="Temperature value"
-              value={inputValue}
-              onChange={(e) => {
-                const v = e.target.value;
-
-                setInputValue(v);
-              }}
-              onKeyDown={(e) => {
-                if (e.key === "Enter" && !isNaN(Number(inputValue))) {
-                  setValue(Number(inputValue));
-                }
-              }}
-            />
-          </Tooltip>
-        </output>
-      )}
-      value={value}
-      onChange={handleChange}
-    />
+    <div className="flex flex-row  max-w-md h-[348px] gap-6 w-full">
+      <Slider   
+        size="sm"
+        step={0.01} 
+        maxValue={1} 
+        minValue={0} 
+        orientation="vertical"
+        aria-label="Temperature"
+        defaultValue={0.2}
+      />
+      <Slider   
+        size="md"
+        step={0.01} 
+        maxValue={1} 
+        minValue={0} 
+        orientation="vertical"
+        aria-label="Temperature"
+        defaultValue={0.4}
+      />
+      <Slider   
+        size="lg"
+        step={0.01} 
+        maxValue={1} 
+        minValue={0} 
+        orientation="vertical"
+        aria-label="Temperature"
+        defaultValue={0.6}
+      />
+    </div> 
   );
 }
 
