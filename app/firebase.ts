230a53f58b0c107a4f996c18a0344f5a6497560a
Berke <berkevolkan0909878909@gmail.com>
2024-12-26 22:21:23 +0300

    fix

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,8 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
-
 const firebaseConfig = {
-  apiKey: process.env.apikey,
+  apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
   projectId: process.env.projectId,
   storageBucket: process.env.storageBucket,
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    env:{
+        apiKey: "AIzaSyAHxDF4ZLMhQHd1PYIeW0ej-mOPhVp4ZVc",
+        authDomain: "panda-386904.firebaseapp.com",
+        projectId: "panda-386904",
+        storageBucket: "panda-386904.firebasestorage.app",
+        messagingSenderId: "501981575828",
+        appId: "1:501981575828:web:9baf4ff0c9b81b1ffe5281",
+        measurementId: "G-9L6DCKYPKE"
+    },
+}
